2d795565baa3519d42cacbb3cea58f9a4ec0c7cf
George Aidonidis <giwrgos_aidonidis@outlook.com>
2015-05-25 23:49:18 +0300

    Project started. Works but with wrong way (episodes are displaying in correct/wrong order?)

---
+++ MoviesApp/MoviesApp/MoviesApp/MoviesApp.Shared/ViewModels/CollectionViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesApp.ViewModels
+{
+    public class CollectionViewModel : ViewModelBase
+    {
+        private Dictionary<Tuple<int, int>, Movie> _dictionaryOfEpisodes;
+
+        public Dictionary<Tuple<int, int>, Movie>  DictionaryOfEpisodes
+        {
+            get { return _dictionaryOfEpisodes; }
+            set
+            {
+                if (value != _dictionaryOfEpisodes)
+                {
+                    _dictionaryOfEpisodes = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public CollectionViewModel()
+        {
+            this.DictionaryOfEpisodes = new Dictionary<Tuple<int, int>, Movie>();
+        }
+
+        public async Task ReadDataFromWeb()
+        {
+            var client = new HttpClient();
+            for (int i = 1; i < 7; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    var respone = await client.GetAsync(new Uri(string.Format("http://www.omdbapi.com/?t=Game%20of%20Thrones&Season={0}&Episode={1}", i, j)));
+                    string _result = await respone.Content.ReadAsStringAsync();
+                    var movie = JsonConvert.DeserializeObject<Movie>(_result);
+
+                    //this.DictionaryOfEpisodes.Add(String.Format("Episode:{0}",j), movie);
+                    this.DictionaryOfEpisodes.Add(new Tuple<int, int>(Convert.ToInt16(movie.Season), Convert.ToInt16(movie.Episode)), movie);
+                }
+            }
+        }
+    }
+}
---
+++ MoviesApp/MoviesApp/MoviesApp/MoviesApp.Shared/ViewModels/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MoviesApp.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
